b97abf1426d108aaf1715a0454482ce0f8610dfa
Guimarold <guilhermemarold@gmail.com>
2024-01-26 18:11:43 -0300

    Create test.rb
    test

---
+++ test.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "Bem-vindo, #{nome} #{sobrenome}! Você tem #{idade} anos."
